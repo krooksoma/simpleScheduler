23d15c367fb34e1c3ffd607fe7ff8ee3e46f565d
krooksoma <romulojusto@gmail.com>
2021-07-01 09:11:59 -0400

    updated js file

--- JavaScript/logic.js
+++ JavaScript/logic.js
@@ -1,55 +1,55 @@
 //create variables to select containers
 $(document).ready(() => {
-    const currentTime = $('#currentDay');
-    const tableOfContents = $('.table');
-    const timeOfDay = $('.time-block');
-    const description = $('.description');
-    const saveBtn = $('.saveBtn');
+    let displayhourNow = $('#currentDay');    
+    let saveBtn = $('.saveBtn');
+    
+       
+    //create self updating time using setInterval function to display within displayhourNow container
 
-    //create self updating time using setInterval function to display within currentTime container
+    setInterval(() => {
+        let displaydisplayhourNow = moment(); 
 
-    window.setInterval(() => {
-        const displayCurrentTime = moment();
-
-        currentTime.text(displayCurrentTime.format("MMM Do, YYYY hh:mm:ss"));
-    }
-        , 1000); //looping every sec to update the time displayed
+        displayhourNow.text(displaydisplayhourNow.format('MMM Do, YYYY hh:mm:ss'));
+    
+    }, 1000); //looping every sec to update the time displayed
 
     // create eventListener for save Btn
 
-        // array created to parse items into it
+    
 
-    let descriptionArray = JSON.parse(localStorage.getItem('descriptionArray').value) || [];
-
-    saveBtn.on('click', (event) => {
+    saveBtn.on('click', () => {
+        let value = $(this).siblings('.description').val();
+        let time = $(this).parent().attr('id');
         //getting values from within .description
-        let value = $(this).siblings('.description').value;
-        event.preventDefault();
-        console.log(typeof value);
-        // creating array to stores values
-
+        for(let i = 9; i < 18; i++){
+            $('#hour-' + i).children('.description').val(localStorage.getItem('.hour-' +i));
+            console.log(i);
+        }
         
-        // push item into array
-        descriptionArray.push(value);
-        console.log(descriptionArray);
+        console.log(value);
+        // setting item value in local Storage
         
-        localStorage.setItem('descriptionArray', JSON.stringify(descriptionArray));
-
-
+        localStorage.setItem(time, value);
     });
 
-    
+   
+   
+      
     // create function to set .description color depending on time now
         // loop thru all hour items to set their color
-    for (let hour = 9; hour < 18; hour++) {
-        let time = "present";
-        if (hour > currentTime) {
-            time = "future";
-        } else if (hour < currentTime) {
-            time = "past";
+
+    let hourNow = moment().format('hh');
+
+
+    for (var hour = 9; hour < 18; hour++) {
+        let time = 'present';
+        if (hour > hourNow) {
+            time = 'future';
+        } else if (hour < hourNow) {
+            time = 'past';
         }
         // select the items with id initials hour-X . with X being any number after the dash sign
-        const timeSlot = $("#hour-" + hour);
+        const timeSlot = $('#hour-' + hour);
             // grabbing from pre-defined values for variable time it sets of the color of the children element accordingly
         if (timeSlot && timeSlot.length) {
             timeSlot.children().eq(1).addClass(time);
